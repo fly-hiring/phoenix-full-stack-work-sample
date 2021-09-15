a85bc05e91e595ecdf0944a4274070746129165e
Angelo Chaves <angeloechaves@gmail.com>
2021-09-15 01:11:24 -0300

    Added live refresh to the Instance page

--- lib/fly_web/live/app_live/allocation.ex
+++ lib/fly_web/live/app_live/allocation.ex
@@ -21,7 +21,9 @@ defmodule FlyWeb.AppLive.Allocation do
 
     # Only make the API call if the websocket is setup. Not on initial render.
     if connected?(socket) do
-      {:ok, fetch_allocation(socket)}
+      # Send the signal immediately
+      Process.send_after(self(), :update, 0)
+      {:ok, socket}
     else
       {:ok, socket}
     end
@@ -31,22 +33,31 @@ defmodule FlyWeb.AppLive.Allocation do
     Fly.Client.config(access_token: session["auth_token"] || System.get_env("FLYIO_ACCESS_TOKEN"))
   end
 
+  @impl true
+  def handle_info(:update, socket) do
+    socket = fetch_allocation(socket)
+
+    # Refresh after 3 seconds
+    Process.send_after(self(), :update, 3000)
+
+    {:noreply, socket}
+  end
+
   defp fetch_allocation(socket) do
     app_name = socket.assigns.app_name
     allocation_id = socket.assigns.allocation_id
 
     case Client.fetch_allocation(app_name, allocation_id, socket.assigns.config) do
       {:ok, status} ->
-        assign(socket, :status, status)
-        assign(socket, :allocation, status["allocation"])
+        socket |> assign(:status, status) |> assign(:allocation, status["allocation"])
 
       {:error, :unauthorized} ->
-        put_flash(socket, :error, "Not authenticated")
+        socket |> put_flash(:error, "Not authenticated")
 
       {:error, reason} ->
         Logger.error("Failed to load allocation. Reason: #{inspect(reason)}")
 
-        put_flash(socket, :error, reason)
+        socket |> put_flash(:error, reason)
     end
   end
 
